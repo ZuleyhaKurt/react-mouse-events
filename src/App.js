fac6adb4e8ffce120140573bf9db3448f6fe9acc
Züleyha Kurt <netice2017@gmail.com>
2022-10-13 22:44:00 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import Form from './componenets/form/Form';
-import FormObject from './componenets/form/FormObject';
+// import Form from './componenets/form/Form';
+// import FormObject from './componenets/form/FormObject';
+import KeyboardEvent  from './componenets/keyboard/KeyboardEvent';
 
 function App() {
   return (
     <div className="container mt-4 text-center">
-      {/* <Form /> */}
-      <FormObject/>
+      {/* <Form />
+      <FormObject/> */}
       
-    {/* <KeyboardEvents /> */}
+    <KeyboardEvent />
     {/* <MouseEvent /> */}
   </div>
   );
---
+++ src/componenets/keyboard/KeyboardEvent.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const KeyboardEvent = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      alert("Please dont enter a number");
+      e.preventDefault();
+      }
+      e.keyCode === 13 && setInputValue("");
+
+  };
+
+    const handleAreaPaste = (e) => {
+        e.target.style.border="2px solid red"
+        // console.log(e.target.value)
+        e.target.value = e.clipboardData.getData("text")
+        e.preventDefault();
+     }
+  return (
+    <div className="container text-center">
+      <h1>CLIPBOARD EVENTS</h1>
+      <input
+              className="form-control"
+              value={inputValue}
+        type="text"
+        onChange={(e) => {
+          e.target.value = e.target.value.toLocaleUpperCase();
+          setInputValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+      />
+
+          <p className="text-start mt-4">{inputValue}</p>
+          
+          <textarea className="form-control" name="area" id="area"
+              rows="10" onPaste={handleAreaPaste}
+          ></textarea>
+    </div>
+  );
+};
+
+export default KeyboardEvent;
