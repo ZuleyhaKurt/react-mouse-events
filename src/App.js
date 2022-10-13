9b5d56b096bfdf80a401ea2631a5131a11293d27
Züleyha Kurt <netice2017@gmail.com>
2022-10-13 20:20:33 +0300

    edit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './componenets/form/Form';
+
+function App() {
+  return (
+    <div className="container mt-4 text-center">
+    <Form />
+    {/* <KeyboardEvents /> */}
+    {/* <MouseEvent /> */}
+  </div>
+  );
+}
+
+export default App;
